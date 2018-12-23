27fde9dbe29ae95e634a93a87ff6162477e31f94
AnnaK87 <anna.kaukh@gmail.com>
2018-12-23 22:14:55 +0400

    code for recording completed

--- FunSoundApp/RecordSoundVC.swift
+++ FunSoundApp/RecordSoundVC.swift
@@ -9,25 +9,28 @@
 import UIKit
 import AVFoundation
 
-class RecordSoundVC: UIViewController {
+class RecordSoundVC: UIViewController, AVAudioRecorderDelegate {
 
     @IBOutlet weak var recordBtn: UIButton!
     @IBOutlet weak var stopBtn: UIButton!
     @IBOutlet weak var recordLbl: UILabel!
     
+    var audioRecorder: AVAudioRecorder!
+    var isAudioRecordingGranted: Bool!
+    var timerMeter: Timer!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        checkRecordPermission()
     }
 
     @IBAction func recordBtnPressed(_ sender: Any) {
-        stopBtn.isEnabled = true
-        recordLbl.text = "Recording..."
+       recordAudio()
     }
     
     @IBAction func stopBtnPressed(_ sender: Any) {
-        
+        stopRecording()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,8 +39,98 @@ class RecordSoundVC: UIViewController {
     }
 
     func recordAudio() {
+        recordBtn.isEnabled = false
+        stopBtn.isEnabled = true
+        recordLbl.text = "Recording..."
+        
+        setupRecorder()
+        audioRecorder.record()
+        timerMeter = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector:#selector(self.updateAudioMeter(timer:)), userInfo:nil, repeats:true)
+    }
+    
+    func stopRecording() {
+        stopBtn.isEnabled = false
+        recordBtn.isEnabled = true
+        recordLbl.text = "Tap to Record"
+        
+        audioRecorder.stop()
+        audioRecorder = nil
+        timerMeter.invalidate()
+        print("recorded successfully")
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: "segueToPlaySoundVC", sender: audioRecorder.url)
+        } else {
+            print("Recording failed")
+        }
+    }
+    
+    func checkRecordPermission() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case AVAudioSession.RecordPermission.granted:
+            isAudioRecordingGranted = true
+            break
+        case AVAudioSession.RecordPermission.denied:
+            isAudioRecordingGranted = false
+            break
+        case AVAudioSession.RecordPermission.undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission({(allowed) in
+                if allowed {
+                    self.isAudioRecordingGranted = true
+                } else {
+                    self.isAudioRecordingGranted = false
+                }
+            })
+            break
+        default:
+            break
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let directory = path[0]
+        print(directory)
+        return directory
         
     }
     
+    func getFileUrl() -> URL {
+        let fileName = "recordedVoice.wav"
+        let filePath = getDocumentsDirectory().appendingPathComponent(fileName)
+        return filePath
+    }
+    
+    func setupRecorder() {
+        if isAudioRecordingGranted {
+            let session = AVAudioSession.sharedInstance()
+            do {
+                try session.setCategory(.playAndRecord, mode: .default, options: .defaultToSpeaker)
+                try session.setActive(true)
+                audioRecorder = try AVAudioRecorder(url: getFileUrl(), settings: [:])
+                audioRecorder.delegate = self
+                audioRecorder.isMeteringEnabled = true
+                audioRecorder.prepareToRecord()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+        else {
+            print("No access to microphone")
+            recordLbl.text = "No access to microphone"
+        }
+    }
+    
+    @objc func updateAudioMeter(timer: Timer) {
+            let hr = Int((audioRecorder.currentTime / 60) / 60)
+            let min = Int(audioRecorder.currentTime / 60)
+            let sec = Int(audioRecorder.currentTime.truncatingRemainder(dividingBy: 60))
+            let totalTimeString = String(format: "%02d:%02d:%02d", hr, min, sec)
+            //recordingTimeLabel.text = totalTimeString
+            print(totalTimeString)
+            audioRecorder.updateMeters()
+    }
 }
 
